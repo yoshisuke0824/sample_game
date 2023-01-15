fafc513b72df3e12419571340aebfaa2430f3f23
吉木旺佑 <j1738.matsue-ac.jp>
2023-01-15 17:02:06 +0900

    サンプルを適用

--- main.rb
+++ main.rb
@@ -1,9 +1,16 @@
-require 'dxopal'
-include DXOpal
+require 'dxopal'; include DXOpal
+Window.width = 300
+Window.height = 300
+Window.bgcolor = C_WHITE
 Window.load_resources do
-  Window.bgcolor = C_BLACK
-
+  x = rand(Window.width)
+  y = rand(Window.height)
+  dx = dy = 2
   Window.loop do
-    Window.draw_font(0, 0, "Hello!", Font.default, color: C_WHITE)
+    Window.draw_circle_fill(x, y, 10, C_RED)
+    dx = -dx if x < 0 || x > Window.width
+    dy = -dy if y < 0 || y > Window.height
+    x += dx
+    y += dy
   end
 end
