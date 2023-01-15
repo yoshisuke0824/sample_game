361ddaeb205f02515cda740b8719c90ec164305f
吉木旺佑 <j1738.matsue-ac.jp>
2023-01-15 19:47:19 +0900

    サンプルからチュートリアル基準に修正

--- main.rb
+++ main.rb
@@ -1,190 +1,172 @@
+require 'dxopal'
 include DXOpal
 
-Image.register(:player, 'images/noschar.png') 
-Image.register(:apple, 'images/apple.png') 
-Image.register(:bomb, 'images/bomb.png') 
-
+GROUND_Y = 400
+# 使用する画像を宣言
+Image.register(:player, 'images/player.png')
+Image.register(:apple, 'images/apple.png')
+Image.register(:bomb, 'images/bomb.png')
+# 使用する効果音を宣言
 Sound.register(:get, 'sounds/get.wav')
 Sound.register(:explosion, 'sounds/explosion.wav')
 
-module AppleCatcher
-  class GameInfo
-    def initialize(player)
-      @player = player
-      @score = 0
-      @game_over = false
-    end
-    attr_accessor :player, :score, :game_over
+# ゲームの状態を記憶するハッシュを追加
+GAME_INFO = {
+  scene: :title,  # 現在のシーン(起動直後は:title)
+  score: 0,
+  time: 0,
+}
+
+class Player < Sprite
+  def initialize
+    x = Window.width / 2
+    y = GROUND_Y - Image[:player].height
+    image = Image[:player]
+    super(x, y, image)
+    # 当たり判定を円で設定(中心x, 中心y, 半径)
+    self.collision = [image.width / 2, image.height / 2, 16]
   end
 
-  def self.game_info; @@game_info; end
-  def self.game_info=(info); @@game_info = info; end
-
-  class Background < Sprite
-    def initialize
-      image = Image.new(640, 480, [128, 255, 255])
-      image.box_fill(0, 0,   640, 480, [128, 255, 255])
-      image.box_fill(0, 400, 640, 480, [0, 128, 0])
-      super(0, 0, image)
+  # 移動処理(xからself.xになった)
+  def update
+    if Input.key_down?(K_LEFT) && self.x > 0
+      self.x -= 8
+    elsif Input.key_down?(K_RIGHT) && self.x < (Window.width - Image[:player].width)
+      self.x += 8
     end
   end
+end
 
-  class ScoreDisp < Sprite
-    def draw
-      score = AppleCatcher.game_info.score
-      Window.draw_font(0, 0, "SCORE: #{score}", Font.default)
-    end
+# アイテムを表すクラスを追加
+class Item < Sprite
+  # imageを引数にとるようにした
+  def initialize(image)
+    x = rand(Window.width - image.width)
+    y = -100
+    super(x, y, image)
+    @speed_y = rand(4) + 4 + (GAME_INFO[:time] / (60 * 5)).to_i
   end
 
-  class Message < Sprite
-    attr_accessor :str
-
-    def draw
-      Window.draw_font(0, 30, "PRESS SPACE TO START", Font.default)
+  def update
+    self.y += @speed_y
+    if self.y > Window.height
+      self.vanish
     end
   end
+end
 
-  class Player < Sprite
-    def initialize
-      @anim_idx = 0
-      @anim_ct = 0
-      @tile_images = Image[:player].slice_tiles(4, 4)  # 4x4
-      @char_images = @tile_images.first(4)
-      super(240, 400-32, @char_images.first)
-    end
-
-    def update
-      @anim_ct += 1
-      @anim_ct = 0 if @anim_ct >= 40
-      self.image = @char_images[@anim_ct/10]
-
-      self.x += Input.x * 8
-      self.x = 640-16 if self.x > 640-16
-      self.x = -16 if self.x < -16
-    end
+# 加点アイテムのクラスを追加
+class Apple < Item
+  def initialize
+    super(Image[:apple])
+    # 衝突範囲を円で設定(中心x, 中心y, 半径)
+    self.collision = [image.width / 2, image.height / 2, 56]
   end
 
-  class Item < Sprite
-    def initialize(img)
-      super(rand(640-32), 0, img)
-      @vy = rand(9)+4
-    end
+  # playerと衝突したとき呼ばれるメソッドを追加
+  def hit
+    Sound[:get].play
+    self.vanish
+    GAME_INFO[:score] += 10
+  end
+end
 
-    def update
-      self.y += @vy
-      if self.y > 480
-        self.vanish
-      elsif self === AppleCatcher.game_info.player
-        self.hit
-      end
-    end
-
-    class Apple < Item
-      def initialize
-        super(Image[:apple])
-        self.collision = [0, 15, 75, 80]
-      end
-
-      def hit
-        Sound[:get].play
-        AppleCatcher.game_info.score += 10
-        self.vanish
-      end
-    end
-
-    class Bomb < Item
-      def initialize
-        super(Image[:bomb])
-        self.collision = [15, 31, 61, 76]
-      end
-
-      def hit
-        Sound[:explosion].play
-        AppleCatcher.game_info.game_over = true
-      end
-    end
+# 妨害アイテムのクラスを追加
+class Bomb < Item
+  def initialize
+    super(Image[:bomb])
+    # 衝突範囲を円で設定(中心x, 中心y, 半径)
+    self.collision = [image.width / 2, image.height / 2, 42]
   end
 
-  class Items < Sprite
-    N = 5
+  # playerと衝突したとき呼ばれるメソッドを追加
+  def hit
+    Sound[:explosion].play
+    self.vanish
+    # スコアを0にするのをやめて、ゲームオーバー画面に遷移するようにした
+    GAME_INFO[:scene] = :game_over
+  end
+end
 
-    def initialize
-      super
-      @items = []
-    end
+# アイテム群を管理するクラスを追加
+class Items
+  N = 5
 
-    def update
-      @items.each{|x| x.update}
-      @items.reject!{|x| x.vanished?}
+  def initialize
+    @items = []
+  end
 
-      (N - @items.size).times do
-        klass = (rand(100) < 80) ? Item::Bomb : Item::Apple
-        @items.push(klass.new)
+  # playerを引数に取るようにした
+  def update(player)
+    @items.each{|x| x.update(player)}
+    # playerとitemsが衝突しているかチェックする。衝突していたらhitメソッドが呼ばれる
+    Sprite.check(player, @items)
+    Sprite.clean(@items)
+
+    (N - @items.size).times do
+      if rand(100) < 40
+        @items.push(Apple.new)
+      else
+        @items.push(Bomb.new)
       end
-    end
-
-    def draw
-      Sprite.draw(@items)
     end
   end
 
-  class Main
-    def initialize
-      @background = AppleCatcher::Background.new
-      @score_disp = AppleCatcher::ScoreDisp.new
-      @message = AppleCatcher::Message.new
-      @player = AppleCatcher::Player.new
-      @items = AppleCatcher::Items.new
-      AppleCatcher.game_info = AppleCatcher::GameInfo.new(@player)
-      @scene = :title
-    end
+  def draw
+    Sprite.draw(@items)
+  end
+end
 
-    def tick
-      __send__("tick_#{@scene}")
-    end
+# ゲーム本体を表すクラス
+class Game
+  def initialize
+    reset
+  end
 
-    def tick_title
-      @message.str = "PRESS SPACE TO START"
-      sprites = [@background, @score_disp, @message]
-      if Input.key_push?(K_SPACE)
-        @scene = :playing
+  # ゲームの状態をリセットする
+  def reset
+    @player = Player.new
+    @items = Items.new
+    GAME_INFO[:score] = 0
+    GAME_INFO[:time] = 0
+  end
+
+  # ゲームを実行する
+  def run
+    Window.loop do
+      Window.draw_box_fill(0, 0, Window.width, GROUND_Y, [128, 255, 255])
+      Window.draw_box_fill(0, GROUND_Y, Window.width, Window.height, [0, 128, 0])
+      Window.draw_font(0, 0, "SCORE: #{GAME_INFO[:score]}", Font.default)
+      Window.draw_font(0, 20, "TIME: #{(GAME_INFO[:time] / 60).round(1)}", Font.default)
+
+      case GAME_INFO[:scene]
+      when :title
+        Window.draw_font(Window.width / 2 - 200, Window.height / 2, "スペースキーを押してスタート", Font.default, option={color: [0,0,0]})
+        if Input.key_push?(K_SPACE)
+          GAME_INFO[:scene] = :playing
+        end
+      when :playing
+        @player.update
+        @items.update(@player)
+        GAME_INFO[:time] += 1
+
+        @player.draw
+        @items.draw
+      when :game_over
+        @player.draw
+        @items.draw
+        Window.draw_font(Window.width / 2 - 250, Window.height / 2, "GAME OVER スペースキーを押してスタート", Font.default, option={color: [0,0,0]})
+        if Input.key_push?(K_SPACE)
+          reset
+          GAME_INFO[:scene] = :playing
+        end
       end
-      Sprite.draw(sprites)
-    end
 
-    def tick_playing
-      sprites = [@background, @items, @player, @score_disp]
-      Sprite.update(sprites)
-      if AppleCatcher.game_info.game_over
-        @scene = :game_over
-      end
-      Sprite.draw(sprites)
-    end
-
-    def tick_game_over
-      @message.str = "PRESS SPACE TO RESTART"
-      sprites = [@background, @items, @player, @score_disp, @message]
-      if Input.key_push?(K_SPACE)
-        @player = AppleCatcher::Player.new
-        @items = AppleCatcher::Items.new
-        AppleCatcher.game_info = AppleCatcher::GameInfo.new(@player)
-        @scene = :playing
-      end
-      Sprite.draw(sprites)
     end
   end
 end
 
 Window.load_resources do
-  main = AppleCatcher::Main.new
-
-  Window.loop do
-    main.tick
-  end
-
-  %x{
-    document.getElementById('pause').addEventListener('click', function(){
-      #{Window.paused? ? Window.resume : Window.pause}
-    });
-  }
+  game = Game.new
+  game.run
 end
